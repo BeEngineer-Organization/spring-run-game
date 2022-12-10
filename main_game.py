008db5861185f8a6f8d475ef2773e80d0f78a6f5
guro-Ishiguro <ryu32349581@gmail.com>
2022-12-10 13:45:06 +0900

    first commit

---
+++ main_game.py
@@ -0,0 +1,107 @@
+from direct.showbase.ShowBase import ShowBase
+from direct.showbase.ShowBaseGlobal import globalClock
+from panel_manager import GameStartScene, GameOverScene
+from model_manager import Scene
+
+MOVE_RIGHT = "move_right"
+MOVE_LEFT = "move_left"
+MOVE_UP = "move_up"
+
+
+class Main(ShowBase):
+    def __init__(self):
+        super().__init__()
+        # プレイヤーの変数
+
+        # ゲーム設定
+
+        # キーボード入力設定
+
+        # 車の設定
+
+        # 車を発生させる時間
+
+        # 当たり判定の設定
+
+        # サウンドの設定
+
+        # 毎フレーム読み込まれる処理を登録
+        self.taskMgr.add(self.update, "update")
+
+        # シーンの設定
+        self.scene = Scene()
+
+        # カメラの初期設定
+        self.disableMouse()
+        self.camera.setPos(0, -8, 3)
+        self.camera.setHpr(0, -10, 0)
+
+        # ゲームスタートシーンの読み込み
+        self.game_start_scene = GameStartScene()
+        self.game_start_scene.start_btn["command"] = self.start
+
+        # ゲームオーバーシーンの読み込み
+        self.game_over_scene = GameOverScene()
+        self.game_over_scene.restart_btn["command"] = self.start
+        self.game_over_scene.quit_btn["command"] = self.quit
+
+        # 初期設定ではゲームオーバーシーンは非表示
+        self.game_over_scene.game_over_screen.hide()
+
+
+    # スタート画面でスタートが押された時にメインに移る関数
+    def start(self):
+        # ゲームスタート時には画面を非表示にする
+        self.game_over_scene.game_over_screen.hide()
+        self.game_start_scene.title_menu.hide()
+        self.game_start_scene.title_menu_backdrop.hide()
+
+        # 効果音の切り替え
+
+        # ゲームフラグをオンにする
+        
+        # プレイヤーのインスタンス化
+
+        # カメラを追随する
+
+        # プレイヤーが初期位置に車を発生させる
+
+        # プレイヤーを物理ワールドにアタッチする
+
+    # 初期化する関数
+    def cleanup(self):
+        self.is_contact = False
+        self.is_game = False
+        for car in self.cars:
+            self.scene.physical_world.removeRigidBody(car.node())
+            car.cleanup()
+        self.cars = []
+        self.camera.reparentTo(base.render)
+        self.scene.physical_world.removeRigidBody(self.player.node())
+        if self.player != None:
+            self.player.cleanup()
+
+    # ゲームオーバー画面でやめるが押された時にメインにスタート画面に移る関数
+    def quit(self):
+        # ゲームオーバー画面を非表示にしてスタート画面を表示する
+        self.game_start_scene.title_menu.show()
+        self.game_start_scene.title_menu_backdrop.show()
+        self.game_over_scene.game_over_screen.hide()
+
+        # 効果音を切り替える
+
+        # 初期化処理
+        self.cleanup()
+
+    # 車を出現させる関数
+    def spawn_car(self, p_y):
+        # 車を物理ワールドにアタッチする
+        pass
+
+    # 毎フレーム行う処理
+    def update(self, task):
+        return task.cont
+
+
+app = Main()
+app.run()
